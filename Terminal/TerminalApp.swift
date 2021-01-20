a00eed55dc6113ab236d52d7425c334c94079528
a.m.student <iabgisbgs@gmail.com>
2021-01-20 08:45:57 -0600

    Initial Commit

---
+++ Terminal/TerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalApp.swift
+//  Terminal
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
